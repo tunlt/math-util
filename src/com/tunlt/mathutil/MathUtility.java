5957217279315c6ba192eead3e19a0fc908586be
unknown <tujthickcuoj@gmail.com>
2021-03-26 07:43:00 +0700

    Update core: using recursion. And GREEN 4EVER

--- src/com/tunlt/mathutil/MathUtility.java
+++ src/com/tunlt/mathutil/MathUtility.java
@@ -22,23 +22,32 @@ public class MathUtility {
     // am khong tinh
     // ham chi nhan tu 0..20! -> long
     //0! = 1! = 1
+//    public static long getFactorial(int n){
+//        if(n<0 || n>20) 
+//            throw new IllegalArgumentException("invalid argument. n must be between 0..20");
+//        if(n==0 || n==1)
+//            return 1; // khoi mat cong tinh
+//        // sure n = 2..20
+//        long result = 1; 
+//        
+//        for (int i = 2; i <= n; i++) 
+//            result *= i;
+//            
+//        
+//        return result;
+////        if(n>0){
+////            return getFactorial(n-1)*n;
+////        }else{            
+////        return 1;
+////        }
+    //write resursion(gọi lại chính mình với qui mô nhỏ hơn, nhỏ đén lúc phải dừng)
     public static long getFactorial(int n){
         if(n<0 || n>20) 
             throw new IllegalArgumentException("invalid argument. n must be between 0..20");
         if(n==0 || n==1)
             return 1; // khoi mat cong tinh
-        // sure n = 2..20
-        long result = 1; 
         
-        for (int i = 2; i <= n; i++) 
-            result *= i;
-            
-        
-        return result;
-//        if(n>0){
-//            return getFactorial(n-1)*n;
-//        }else{            
-//        return 1;
-//        }
+        return n*getFactorial(n-1);
+                  
     }
 }
